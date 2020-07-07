3b00c496f5972eddbde66dec1e1d391f575e675e
saranraj.shri <shrisaranraj2001@gmail.com>
2020-07-06 20:26:23 -0400

    Added Bump Chart

--- src/screens/Dashboard/Tabs/Overview/Overview.js
+++ src/screens/Dashboard/Tabs/Overview/Overview.js
@@ -5,6 +5,7 @@ import "./Overview.css";
 
 // Line Chart
 import { ResponsiveLine } from "@nivo/line";
+import { ResponsiveBump } from "@nivo/bump";
 
 // Font Awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -35,6 +36,309 @@ const Overview = () => {
         {
           x: "subway",
           y: 294,
+        },
+      ],
+    },
+  ]);
+
+  const [barData, setBarData] = useState([
+    {
+      id: "Serie 1",
+      data: [
+        {
+          x: 2000,
+          y: 12,
+        },
+        {
+          x: 2001,
+          y: 1,
+        },
+        {
+          x: 2002,
+          y: 6,
+        },
+        {
+          x: 2003,
+          y: 9,
+        },
+        {
+          x: 2004,
+          y: 6,
+        },
+      ],
+    },
+    {
+      id: "Serie 2",
+      data: [
+        {
+          x: 2000,
+          y: 11,
+        },
+        {
+          x: 2001,
+          y: 2,
+        },
+        {
+          x: 2002,
+          y: 12,
+        },
+        {
+          x: 2003,
+          y: 8,
+        },
+        {
+          x: 2004,
+          y: 10,
+        },
+      ],
+    },
+    {
+      id: "Serie 3",
+      data: [
+        {
+          x: 2000,
+          y: 8,
+        },
+        {
+          x: 2001,
+          y: 7,
+        },
+        {
+          x: 2002,
+          y: 8,
+        },
+        {
+          x: 2003,
+          y: 6,
+        },
+        {
+          x: 2004,
+          y: 4,
+        },
+      ],
+    },
+    {
+      id: "Serie 4",
+      data: [
+        {
+          x: 2000,
+          y: 1,
+        },
+        {
+          x: 2001,
+          y: 12,
+        },
+        {
+          x: 2002,
+          y: 7,
+        },
+        {
+          x: 2003,
+          y: 10,
+        },
+        {
+          x: 2004,
+          y: 2,
+        },
+      ],
+    },
+    {
+      id: "Serie 5",
+      data: [
+        {
+          x: 2000,
+          y: 10,
+        },
+        {
+          x: 2001,
+          y: 5,
+        },
+        {
+          x: 2002,
+          y: 10,
+        },
+        {
+          x: 2003,
+          y: 7,
+        },
+        {
+          x: 2004,
+          y: 11,
+        },
+      ],
+    },
+    {
+      id: "Serie 6",
+      data: [
+        {
+          x: 2000,
+          y: 2,
+        },
+        {
+          x: 2001,
+          y: 10,
+        },
+        {
+          x: 2002,
+          y: 11,
+        },
+        {
+          x: 2003,
+          y: 5,
+        },
+        {
+          x: 2004,
+          y: 7,
+        },
+      ],
+    },
+    {
+      id: "Serie 7",
+      data: [
+        {
+          x: 2000,
+          y: 3,
+        },
+        {
+          x: 2001,
+          y: 9,
+        },
+        {
+          x: 2002,
+          y: 3,
+        },
+        {
+          x: 2003,
+          y: 1,
+        },
+        {
+          x: 2004,
+          y: 9,
+        },
+      ],
+    },
+    {
+      id: "Serie 8",
+      data: [
+        {
+          x: 2000,
+          y: 9,
+        },
+        {
+          x: 2001,
+          y: 11,
+        },
+        {
+          x: 2002,
+          y: 2,
+        },
+        {
+          x: 2003,
+          y: 3,
+        },
+        {
+          x: 2004,
+          y: 12,
+        },
+      ],
+    },
+    {
+      id: "Serie 9",
+      data: [
+        {
+          x: 2000,
+          y: 7,
+        },
+        {
+          x: 2001,
+          y: 8,
+        },
+        {
+          x: 2002,
+          y: 1,
+        },
+        {
+          x: 2003,
+          y: 4,
+        },
+        {
+          x: 2004,
+          y: 1,
+        },
+      ],
+    },
+    {
+      id: "Serie 10",
+      data: [
+        {
+          x: 2000,
+          y: 5,
+        },
+        {
+          x: 2001,
+          y: 4,
+        },
+        {
+          x: 2002,
+          y: 5,
+        },
+        {
+          x: 2003,
+          y: 11,
+        },
+        {
+          x: 2004,
+          y: 3,
+        },
+      ],
+    },
+    {
+      id: "Serie 11",
+      data: [
+        {
+          x: 2000,
+          y: 4,
+        },
+        {
+          x: 2001,
+          y: 6,
+        },
+        {
+          x: 2002,
+          y: 9,
+        },
+        {
+          x: 2003,
+          y: 2,
+        },
+        {
+          x: 2004,
+          y: 5,
+        },
+      ],
+    },
+    {
+      id: "Serie 12",
+      data: [
+        {
+          x: 2000,
+          y: 6,
+        },
+        {
+          x: 2001,
+          y: 3,
+        },
+        {
+          x: 2002,
+          y: 4,
+        },
+        {
+          x: 2003,
+          y: 12,
+        },
+        {
+          x: 2004,
+          y: 8,
         },
       ],
     },
@@ -212,6 +516,54 @@ const Overview = () => {
           <p className="table-text">
             3K+ <b>Users</b>
           </p>
+        </div>
+      </div>
+
+      {/* Bar Chart */}
+      <div className="grid-container">
+        <div className="grid-item" style={{ height: 300, marginTop: 20 }}>
+          <h5 className="title">Emotions among people (Jan)</h5>
+          <ResponsiveBump
+            data={barData}
+            margin={{ top: 20, right: 100, bottom: 40, left: 60 }}
+            colors={{ scheme: "spectral" }}
+            lineWidth={3}
+            activeLineWidth={6}
+            inactiveLineWidth={3}
+            inactiveOpacity={0.15}
+            pointSize={10}
+            activePointSize={16}
+            inactivePointSize={0}
+            pointColor={{ theme: "background" }}
+            pointBorderWidth={3}
+            activePointBorderWidth={3}
+            pointBorderColor={{ from: "serie.color" }}
+            axisTop={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "",
+              legendPosition: "middle",
+              legendOffset: -36,
+            }}
+            axisRight={null}
+            axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "",
+              legendPosition: "middle",
+              legendOffset: 32,
+            }}
+            axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "ranking",
+              legendPosition: "middle",
+              legendOffset: -40,
+            }}
+          />
         </div>
       </div>
     </div>
