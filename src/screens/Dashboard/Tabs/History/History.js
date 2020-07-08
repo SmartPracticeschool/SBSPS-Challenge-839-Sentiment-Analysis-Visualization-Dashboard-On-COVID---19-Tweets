ce0d9c504cac3cf11be518e92390999dcc58acaf
Silver <silver@Silver-Soul>
2020-07-08 16:41:01 +0530

    Creates History component screen

---
+++ src/screens/Dashboard/Tabs/History/History.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <div>
+      <div>
+        <p>History of sentiments throughout the month</p>
+      </div>
+    </div>
+  );
+};
+
+export default History;
--- src/screens/Dashboard/Tabs/index.js
+++ src/screens/Dashboard/Tabs/index.js
@@ -1,4 +1,5 @@
 import Overview from "./Overview/Overview";
 import TodayReport from "./TodayReport/TodayReport";
+import History from "./History/History";
 
-export { Overview, TodayReport };
+export { Overview, TodayReport, History };
